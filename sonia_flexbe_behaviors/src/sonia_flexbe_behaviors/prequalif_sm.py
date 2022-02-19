f8cbe21db79b6347976210f883dfaeaedb9bf7e2
supertoto29 <francisalonzo29@gmail.com>
2022-02-19 14:50:21 -0500

    Prequalif mission.

--- sonia_flexbe_behaviors/src/sonia_flexbe_behaviors/prequalif_sm.py
+++ sonia_flexbe_behaviors/src/sonia_flexbe_behaviors/prequalif_sm.py
@@ -11,6 +11,7 @@ from flexbe_core import Behavior, Autonomy, OperatableStateMachine, ConcurrencyC
 from sonia_flexbe_states.add_pose_to_trajectory import add_pose_to_trajectory
 from sonia_flexbe_states.init_trajectory import init_trajectory
 from sonia_flexbe_states.send_to_planner import send_to_planner
+from sonia_flexbe_states.wait_mission import wait_mission
 from sonia_flexbe_states.wait_target_reached import wait_target_reached
 # Additional imports can be added inside the following tags
 # [MANUAL_IMPORT]
@@ -34,6 +35,7 @@ class PrequalifSM(Behavior):
 
 		# parameters of this behavior
 		self.add_parameter('angle_gate', 0)
+		self.add_parameter('distance_start_loop', 7.572)
 
 		# references to used behaviors
 
@@ -47,7 +49,7 @@ class PrequalifSM(Behavior):
 
 
 	def create(self):
-		# x:732 y:653, x:726 y:503, x:1162 y:459
+		# x:732 y:653, x:1103 y:495, x:938 y:344
 		_state_machine = OperatableStateMachine(outcomes=['finished', 'error', 'failed'])
 
 		# Additional creation code can be added inside the following tags
@@ -57,123 +59,115 @@ class PrequalifSM(Behavior):
 
 
 		with _state_machine:
-			# x:105 y:56
-			OperatableStateMachine.add('init_traj',
-										init_trajectory(),
-										transitions={'continue': 'pose 1'},
-										autonomy={'continue': Autonomy.Off},
-										remapping={'trajectory': 'empty_traj'})
+			# x:39 y:98
+			OperatableStateMachine.add('mission_switch',
+										wait_mission(),
+										transitions={'continue': 'init_traj', 'failed': 'failed'},
+										autonomy={'continue': Autonomy.Off, 'failed': Autonomy.Off})
 
-			# x:350 y:50
+			# x:378 y:42
 			OperatableStateMachine.add('pose 1',
-										add_pose_to_trajectory(positionX=0, positionY=0, positionZ=1.5, orientationX=0, orientationY=0, orientationZ=self.angle_gate, frame=2, speed=0, precision=0, rotation=True),
+										add_pose_to_trajectory(positionX=0, positionY=0, positionZ=1, orientationX=0, orientationY=0, orientationZ=self.angle_gate, frame=2, speed=0, precision=0, rotation=True),
 										transitions={'continue': 'pose 2'},
 										autonomy={'continue': Autonomy.Off},
 										remapping={'input_traj': 'empty_traj', 'trajectory': 'traj1'})
 
-			# x:74 y:356
+			# x:79 y:303
 			OperatableStateMachine.add('pose 10',
-										add_pose_to_trajectory(positionX=0, positionY=0, positionZ=0, orientationX=0, orientationY=0, orientationZ=0, frame=0, speed=1, precision=0, rotation=True),
+										add_pose_to_trajectory(positionX=3.849, positionY=-1.364, positionZ=0.304, orientationX=0, orientationY=0, orientationZ=-39.23, frame=1, speed=1, precision=0, rotation=True),
 										transitions={'continue': 'pose 11'},
 										autonomy={'continue': Autonomy.Off},
 										remapping={'input_traj': 'traj9', 'trajectory': 'traj10'})
 
-			# x:71 y:504
+			# x:81 y:401
 			OperatableStateMachine.add('pose 11',
-										add_pose_to_trajectory(positionX=0, positionY=0, positionZ=0, orientationX=0, orientationY=0, orientationZ=0, frame=0, speed=1, precision=0, rotation=True),
+										add_pose_to_trajectory(positionX=0, positionY=0, positionZ=0, orientationX=0, orientationY=0, orientationZ=0, frame=1, speed=1, precision=0, rotation=True),
 										transitions={'continue': 'pose 12'},
 										autonomy={'continue': Autonomy.Off},
 										remapping={'input_traj': 'traj10', 'trajectory': 'traj11'})
 
-			# x:330 y:507
+			# x:317 y:393
 			OperatableStateMachine.add('pose 12',
-										add_pose_to_trajectory(positionX=0, positionY=0, positionZ=0, orientationX=0, orientationY=0, orientationZ=0, frame=0, speed=1, precision=0, rotation=True),
-										transitions={'continue': 'pose 13'},
+										add_pose_to_trajectory(positionX=8.072, positionY=0, positionZ=0, orientationX=0, orientationY=0, orientationZ=0, frame=1, speed=1, precision=0, rotation=True),
+										transitions={'continue': 'send to planner'},
 										autonomy={'continue': Autonomy.Off},
 										remapping={'input_traj': 'traj11', 'trajectory': 'traj12'})
 
-			# x:343 y:347
-			OperatableStateMachine.add('pose 13',
-										add_pose_to_trajectory(positionX=0, positionY=0, positionZ=0, orientationX=0, orientationY=0, orientationZ=0, frame=0, speed=1, precision=0, rotation=True),
-										transitions={'continue': 'pose 14'},
-										autonomy={'continue': Autonomy.Off},
-										remapping={'input_traj': 'traj12', 'trajectory': 'traj13'})
-
-			# x:622 y:354
-			OperatableStateMachine.add('pose 14',
-										add_pose_to_trajectory(positionX=0, positionY=0, positionZ=0, orientationX=0, orientationY=0, orientationZ=0, frame=0, speed=1, precision=0, rotation=True),
-										transitions={'continue': 'send to planner'},
-										autonomy={'continue': Autonomy.Off},
-										remapping={'input_traj': 'traj13', 'trajectory': 'traj'})
-
-			# x:612 y:46
+			# x:563 y:41
 			OperatableStateMachine.add('pose 2',
-										add_pose_to_trajectory(positionX=0.5, positionY=0, positionZ=2, orientationX=0, orientationY=0, orientationZ=0, frame=3, speed=1, precision=0, rotation=True),
+										add_pose_to_trajectory(positionX=0.5, positionY=0, positionZ=1, orientationX=0, orientationY=0, orientationZ=0, frame=1, speed=1, precision=0, rotation=True),
 										transitions={'continue': 'pose 3'},
 										autonomy={'continue': Autonomy.Off},
 										remapping={'input_traj': 'traj1', 'trajectory': 'traj2'})
 
-			# x:868 y:42
+			# x:749 y:41
 			OperatableStateMachine.add('pose 3',
-										add_pose_to_trajectory(positionX=8.07202, positionY=0, positionZ=0, orientationX=0, orientationY=0, orientationZ=0, frame=3, speed=1, precision=0, rotation=True),
+										add_pose_to_trajectory(positionX=self.distance_start_loop, positionY=0, positionZ=0, orientationX=0, orientationY=0, orientationZ=0, frame=1, speed=1, precision=0, rotation=True),
 										transitions={'continue': 'pose4'},
 										autonomy={'continue': Autonomy.Off},
 										remapping={'input_traj': 'traj2', 'trajectory': 'traj3'})
 
-			# x:1153 y:203
+			# x:901 y:200
 			OperatableStateMachine.add('pose 5',
-										add_pose_to_trajectory(positionX=0, positionY=0, positionZ=0, orientationX=0, orientationY=0, orientationZ=0, frame=3, speed=1, precision=0, rotation=True),
+										add_pose_to_trajectory(positionX=3.849, positionY=-1.364, positionZ=-0.304, orientationX=0, orientationY=0, orientationZ=-39.923, frame=1, speed=1, precision=0, rotation=True),
 										transitions={'continue': 'pose 6'},
 										autonomy={'continue': Autonomy.Off},
 										remapping={'input_traj': 'traj4', 'trajectory': 'traj5'})
 
-			# x:876 y:194
+			# x:716 y:202
 			OperatableStateMachine.add('pose 6',
-										add_pose_to_trajectory(positionX=0, positionY=0, positionZ=0, orientationX=0, orientationY=0, orientationZ=0, frame=0, speed=0, precision=0, rotation=True),
+										add_pose_to_trajectory(positionX=1.079, positionY=0.383, positionZ=-0.196, orientationX=0, orientationY=0, orientationZ=39.23, frame=1, speed=0, precision=0, rotation=True),
 										transitions={'continue': 'pose 7'},
 										autonomy={'continue': Autonomy.Off},
 										remapping={'input_traj': 'traj5', 'trajectory': 'traj6'})
 
-			# x:611 y:189
+			# x:503 y:201
 			OperatableStateMachine.add('pose 7',
-										add_pose_to_trajectory(positionX=0, positionY=0, positionZ=0, orientationX=0, orientationY=0, orientationZ=0, frame=0, speed=1, precision=0, rotation=True),
+										add_pose_to_trajectory(positionX=1.75, positionY=1.721, positionZ=-0.314, orientationX=0, orientationY=0, orientationZ=90, frame=1, speed=1, precision=0, rotation=True),
 										transitions={'continue': 'pose 8'},
 										autonomy={'continue': Autonomy.Off},
 										remapping={'input_traj': 'traj6', 'trajectory': 'traj7'})
 
-			# x:359 y:190
+			# x:281 y:200
 			OperatableStateMachine.add('pose 8',
-										add_pose_to_trajectory(positionX=0, positionY=0, positionZ=0, orientationX=0, orientationY=0, orientationZ=0, frame=0, speed=1, precision=0, rotation=True),
+										add_pose_to_trajectory(positionX=1.721, positionY=1.75, positionZ=0.314, orientationX=0, orientationY=0, orientationZ=90, frame=1, speed=1, precision=0, rotation=True),
 										transitions={'continue': 'pose 9'},
 										autonomy={'continue': Autonomy.Off},
 										remapping={'input_traj': 'traj7', 'trajectory': 'traj8'})
 
 			# x:77 y:201
 			OperatableStateMachine.add('pose 9',
-										add_pose_to_trajectory(positionX=0, positionY=0, positionZ=0, orientationX=0, orientationY=0, orientationZ=0, frame=0, speed=0, precision=0, rotation=True),
+										add_pose_to_trajectory(positionX=1.079928, positionY=0.3833965, positionZ=0.19600, orientationX=0, orientationY=0, orientationZ=39.23, frame=1, speed=0, precision=0, rotation=True),
 										transitions={'continue': 'pose 10'},
 										autonomy={'continue': Autonomy.Off},
 										remapping={'input_traj': 'traj8', 'trajectory': 'traj9'})
 
-			# x:1151 y:40
+			# x:933 y:40
 			OperatableStateMachine.add('pose4',
-										add_pose_to_trajectory(positionX=11.921, positionY=-1.364, positionZ=2, orientationX=0, orientationY=0, orientationZ=-39.23, frame=3, speed=1, precision=0, rotation=True),
+										add_pose_to_trajectory(positionX=0, positionY=0, positionZ=0, orientationX=0, orientationY=0, orientationZ=0, frame=1, speed=1, precision=0, rotation=True),
 										transitions={'continue': 'pose 5'},
 										autonomy={'continue': Autonomy.Off},
 										remapping={'input_traj': 'traj3', 'trajectory': 'traj4'})
 
-			# x:885 y:364
+			# x:573 y:393
 			OperatableStateMachine.add('send to planner',
 										send_to_planner(),
 										transitions={'continue': 'target_reached', 'failed': 'failed'},
 										autonomy={'continue': Autonomy.Off, 'failed': Autonomy.Off},
-										remapping={'input_traj': 'traj'})
+										remapping={'input_traj': 'traj12'})
 
-			# x:890 y:543
+			# x:893 y:485
 			OperatableStateMachine.add('target_reached',
 										wait_target_reached(),
 										transitions={'target_reached': 'finished', 'target_not_reached': 'failed', 'error': 'error'},
 										autonomy={'target_reached': Autonomy.Off, 'target_not_reached': Autonomy.Off, 'error': Autonomy.Off})
+
+			# x:205 y:44
+			OperatableStateMachine.add('init_traj',
+										init_trajectory(),
+										transitions={'continue': 'pose 1'},
+										autonomy={'continue': Autonomy.Off},
+										remapping={'trajectory': 'empty_traj'})
 
 
 		return _state_machine
