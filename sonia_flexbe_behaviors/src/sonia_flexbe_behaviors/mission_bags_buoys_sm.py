440717e071383682e1af24d94c79504441241863
Willy <willy.kao.1@ens.etsmtl.ca>
2022-05-21 15:23:24 -0400

    adjusted missions for bags

--- sonia_flexbe_behaviors/src/sonia_flexbe_behaviors/mission_bags_buoys_sm.py
+++ sonia_flexbe_behaviors/src/sonia_flexbe_behaviors/mission_bags_buoys_sm.py
@@ -15,6 +15,8 @@ from sonia_navigation_states.send_to_planner import send_to_planner
 from sonia_navigation_states.set_control_mode import set_control_mode
 from sonia_navigation_states.wait_target_reached import wait_target_reached
 from sonia_navigation_states.yaw_orbit_from_given_point import yaw_orbit_from_given_point
+from sonia_vision_states.start_rosbag_record import start_rosbag_record
+from sonia_vision_states.stop_rosbag_record import stop_rosbag_record
 # Additional imports can be added inside the following tags
 # [MANUAL_IMPORT]
 
@@ -40,6 +42,7 @@ class mission_bags_buoysSM(Behavior):
 		self.add_parameter('initial_backward_length', 0)
 		self.add_parameter('backward_length_in_between_arcs', 0)
 		self.add_parameter('arc_angle', 0)
+		self.add_parameter('orbit_speed', 0)
 
 		# references to used behaviors
 
@@ -106,42 +109,42 @@ class mission_bags_buoysSM(Behavior):
 
 			# x:311 y:292
 			OperatableStateMachine.add('go_left_in_orbit_1',
-										yaw_orbit_from_given_point(pointX=self.initial_backward_length, pointY=0, rotation=self.arc_angle/2, speed=1),
+										yaw_orbit_from_given_point(pointX=self.initial_backward_length, pointY=0, rotation=self.arc_angle/2, speed=self.orbit_speed),
 										transitions={'continue': 'send_to_planner'},
 										autonomy={'continue': Autonomy.Off},
 										remapping={'input_traj': 'traj_backward_1', 'trajectory': 'traj_left_orbit_1'})
 
 			# x:521 y:308
 			OperatableStateMachine.add('go_left_in_orbit_2',
-										yaw_orbit_from_given_point(pointX=self.initial_backward_length+self.backward_length_in_between_arcs, pointY=0, rotation=self.arc_angle, speed=1),
+										yaw_orbit_from_given_point(pointX=self.initial_backward_length+self.backward_length_in_between_arcs, pointY=0, rotation=self.arc_angle, speed=self.orbit_speed),
 										transitions={'continue': 'go_backward_3'},
 										autonomy={'continue': Autonomy.Off},
 										remapping={'input_traj': 'traj_backward_2', 'trajectory': 'traj_left_orbit_2'})
 
 			# x:746 y:128
 			OperatableStateMachine.add('go_left_in_orbit_3',
-										yaw_orbit_from_given_point(pointX=self.initial_backward_length+self.backward_length_in_between_arcs+self.backward_length_in_between_arcs+self.backward_length_in_between_arcs, pointY=0, rotation=self.arc_angle, speed=1),
+										yaw_orbit_from_given_point(pointX=self.initial_backward_length+self.backward_length_in_between_arcs+self.backward_length_in_between_arcs+self.backward_length_in_between_arcs, pointY=0, rotation=self.arc_angle, speed=self.orbit_speed),
 										transitions={'continue': 'go_backward_5'},
 										autonomy={'continue': Autonomy.Off},
 										remapping={'input_traj': 'traj_backward_4', 'trajectory': 'traj_left_orbit_3'})
 
 			# x:522 y:122
 			OperatableStateMachine.add('go_right_in_orbit_1',
-										yaw_orbit_from_given_point(pointX=self.initial_backward_length, pointY=0, rotation=-self.arc_angle, speed=1),
+										yaw_orbit_from_given_point(pointX=self.initial_backward_length, pointY=0, rotation=-self.arc_angle, speed=self.orbit_speed),
 										transitions={'continue': 'go_backward_2'},
 										autonomy={'continue': Autonomy.Off},
 										remapping={'input_traj': 'traj_2', 'trajectory': 'traj_right_orbit_1'})
 
 			# x:519 y:492
 			OperatableStateMachine.add('go_right_in_orbit_2',
-										yaw_orbit_from_given_point(pointX=self.initial_backward_length+self.backward_length_in_between_arcs+self.backward_length_in_between_arcs, pointY=0, rotation=-self.arc_angle, speed=1),
+										yaw_orbit_from_given_point(pointX=self.initial_backward_length+self.backward_length_in_between_arcs+self.backward_length_in_between_arcs, pointY=0, rotation=-self.arc_angle, speed=self.orbit_speed),
 										transitions={'continue': 'go_backward_4'},
 										autonomy={'continue': Autonomy.Off},
 										remapping={'input_traj': 'traj_backward_3', 'trajectory': 'traj_right_orbit_2'})
 
 			# x:738 y:333
 			OperatableStateMachine.add('go_right_in_orbit_3',
-										yaw_orbit_from_given_point(pointX=self.initial_backward_length+self.backward_length_in_between_arcs+self.backward_length_in_between_arcs+self.backward_length_in_between_arcs+self.backward_length_in_between_arcs, pointY=0, rotation=-self.arc_angle, speed=1),
+										yaw_orbit_from_given_point(pointX=self.initial_backward_length+self.backward_length_in_between_arcs+self.backward_length_in_between_arcs+self.backward_length_in_between_arcs+self.backward_length_in_between_arcs, pointY=0, rotation=-self.arc_angle, speed=self.orbit_speed),
 										transitions={'continue': 'send_to_planner_final'},
 										autonomy={'continue': Autonomy.Off},
 										remapping={'input_traj': 'traj_backward_5', 'trajectory': 'traj_right_orbit_3'})
@@ -180,16 +183,30 @@ class mission_bags_buoysSM(Behavior):
 										transitions={'continue': 'init_traj', 'failed': 'failed'},
 										autonomy={'continue': Autonomy.Off, 'failed': Autonomy.Off})
 
+			# x:278 y:517
+			OperatableStateMachine.add('start_bag_record',
+										start_rosbag_record(bag_name='bag_test', topic_name='/proc_simulation/front/compressed', timer_split=0, record_path='/home/willy'),
+										transitions={'continue': 'init_traj_2', 'failed': 'failed'},
+										autonomy={'continue': Autonomy.Off, 'failed': Autonomy.Off},
+										remapping={'rosbag_proc': 'rosbag_proc', 'command': 'command'})
+
+			# x:781 y:596
+			OperatableStateMachine.add('stop_rosbag_record',
+										stop_rosbag_record(),
+										transitions={'continue': 'finished', 'failed': 'failed'},
+										autonomy={'continue': Autonomy.Off, 'failed': Autonomy.Off},
+										remapping={'rosbag_proc': 'rosbag_proc', 'command': 'command'})
+
 			# x:310 y:455
 			OperatableStateMachine.add('wait_target_reached',
 										wait_target_reached(),
-										transitions={'target_reached': 'init_traj_2', 'target_not_reached': 'failed', 'error': 'failed'},
+										transitions={'target_reached': 'start_bag_record', 'target_not_reached': 'failed', 'error': 'failed'},
 										autonomy={'target_reached': Autonomy.Off, 'target_not_reached': Autonomy.Off, 'error': Autonomy.Off})
 
 			# x:740 y:522
 			OperatableStateMachine.add('wait_target_reached_final',
 										wait_target_reached(),
-										transitions={'target_reached': 'finished', 'target_not_reached': 'failed', 'error': 'failed'},
+										transitions={'target_reached': 'stop_rosbag_record', 'target_not_reached': 'failed', 'error': 'failed'},
 										autonomy={'target_reached': Autonomy.Off, 'target_not_reached': Autonomy.Off, 'error': Autonomy.Off})
 
 			# x:312 y:206
