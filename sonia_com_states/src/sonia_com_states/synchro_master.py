79280707056dbae6dd0914409c70f4b57c21ac30
supertoto29 <francisalonzo29@gmail.com>
2022-06-16 20:53:03 -0400

    synchro master wip

--- sonia_com_states/src/sonia_com_states/synchro_master.py
+++ sonia_com_states/src/sonia_com_states/synchro_master.py
@@ -5,8 +5,10 @@ import rospy
 from time import time
 
 from flexbe_core import EventState, Logger
-from std_msgs.msg import Int8MultiArray
+from std_msgs.msg import Int8MultiArray, Float32, Bool
+from std_srvs.srv import Empty
 from nav_msgs.msg import Odometry
+from geometry_msgs.msg import Point, Vector3
 from sonia_common.msg import AddPose
 
 class synchro_master(EventState):
@@ -15,37 +17,91 @@ class synchro_master(EventState):
         Synchronize a mission with the slave. The master changes depth to reduce the
         chance of hitting each submarine.
 
-        -- mission_id           UInt8       Position of the mission in the array for the previous mission
+        -- mission_id           UInt8       Position of the mission in the array for the mission to sync
 
         #> depth_change          AddPose    Pose for the new depth requested for the submarine
 
         <= continue                         Both mission completed.
         <= failed                           Mission failed or already completed by the slave
         <= timeout                          Timeout reached. The other submarine didn't complete the mission
+        <= pose_change_depth                The request to change the depth with the output_key "depth_change"
     '''
 
-    def __init__(self, depth_change=True,  mission_id=0, timeout=300):
-        super(synchro_master, self).__init__(outcomes=['continue', 'failed', 'timeout'])
+    def __init__(self, depth_change=True, max_depth_surface=0.5, max_depth_bottom=1.5, min_depth_offset=1,  mission_id=0, timeout=180):
+        super(synchro_master, self).__init__(outcomes=['continue', 'failed', 'timeout', 'pose_change_depth'],
+                                             output_keys=['depth_change'])
+        self.array = Int8MultiArray()
         self.other_array = Int8MultiArray()
         self.depth_change = depth_change
-        self.mission_id = mission_id
+        self.max_depth_surface = max_depth_surface
+        self.max_depth_bottom = max_depth_bottom
+        self.min_depth_offset = min_depth_offset
+        self.mission_id = mission_id - 1
         self.timeout = timeout
         self.actual_z = 0
-        self.message_received_other = False
+        self.other_sub_z = 0
+        self.request_completed = False
+        self.depth_received = False
+        self.other_sub_depth_received = False
+        self.done_already = False
+        self.failed = False
+        self.mission_to_do = False
+        self.sync_in_progress = False
+
+    def get_depth_cb(self, data):
+        self.other_sub_z = data.data
+        self.other_sub_depth_received = True
 
     def get_current_position_cb(self, data):
         self.actual_z = data.pose.pose.position.z
+        self.depth_received = True
 
-    def profondeur_verification(self):
-        # self.depth_request = rospy.ServiceProxy()
-        # try:
-        #     self.depth_request()
+    def profondeur_change_request(self):
+        Logger.log('Other submarine depth is ' + str(self.other_sub_z) + ' and this submarine is ' + \
+            str(self.actual_z), Logger.REPORT_HINT)
 
-        # except rospy.ServiceException as exc:
-        #     Logger.log('Service did not process request: ' + str(exc), Logger.REPORT_HINT)
-        #     self.failed = True
+        # Z Difference used for calculation
+        zdiff = self.other_sub_z - self.actual_z
 
-        Logger.log('Other submarine depth is ' + str() + ' and this submarine is ')
+        #Creating the pose for the trajectory
+        pose = AddPose()
+        pose.position = Point(0, 0, 0)
+        pose.orientation = Vector3(0, 0, 0)
+        pose.frame = 1
+        pose.speed = 0
+        pose.fine = 0
+        pose.rotation = False
+
+        if abs(zdiff) >= self.min_depth_offset:
+            Logger.log('The submarines are at a good depth difference. No need to move to move it.', Logger.REPORT_HINT)
+        else:
+            Logger.log('The difference is less than ' + str(self.min_depth_offset) + 'm. Move is needed.', Logger.REPORT_HINT)
+            
+            # The master is over the other submarine
+            if zdiff > 0:
+                Logger.log('Trying to go up.', Logger.REPORT_HINT)
+                if self.actual_z - self.min_depth_offset + zdiff < self.max_depth_surface:
+                    Logger.log('The submarine is too close of the surface and could hit the other sub going down.', Logger.REPORT_HINT)
+                    self.failed = True
+                else:
+                    # The goal is to keep the min_depth_offset between the submarine
+                    pose.position.z = -self.min_depth_offset + zdiff
+
+            # The master is under the other submarine
+            elif zdiff < 0:
+                Logger.log('Trying to go down', Logger.REPORT_HINT)
+                if self.actual_z + self.min_depth_offset - zdiff > self.max_depth_bottom:
+                    Logger.log('The submarine is too close to the bottom and could hit the other sub going up.', Logger.REPORT_HINT)
+                    self.failed = True
+                else:
+                    # The goal is to keep the min_depth_offset between the submarine
+                    pose.position.z = self.min_depth_offset - zdiff
+            
+            # This state should not be able to happend but this has been implemented as a safety measure
+            else:
+                self.failed = True
+
+        return pose
 
     def other_mission_array_cb(self, msg):
         Logger.log('Received the updated state of the other sub', Logger.REPORT_HINT)
@@ -63,24 +119,35 @@ class synchro_master(EventState):
 
             # Vérification de la profondeur de l'autre sous-marin
             if self.depth_change == True:
-            self.get_current_position_sub = rospy.Subscriber('/telemetry/auv_states', Odometry, self.get_current_position_cb)
-            
-            
+                Logger.log('Verification from the other sub depth', Logger.REPORT_HINT)
+                self.get_current_position_sub = rospy.Subscriber('/telemetry/auv_states', Odometry, self.get_current_position_cb)
+                self.get_depth_other_sub = rospy.Subscriber('/proc_underwater_com/other_sub_depth', Float32, self.get_depth_cb)
+                
+                # Demande pour obtenir la profondeur de l'autre sous-marin
+                self.depth_request = rospy.ServiceProxy('/proc_underwater_com/depth_request', Empty)
+                try:
+                    self.depth_request()
+                    Logger.log('Request for the other sub depth complete', Logger.REPORT_HINT)
+                    self.request_completed = True
 
-
-
+                except rospy.ServiceException as exc:
+                    Logger.log('Service did not process request: ' + str(exc), Logger.REPORT_HINT)
+                    self.failed = True
+                
         if msg.data[self.mission_id] > 1 and self.mission_to_do == True:
-            Logger.log('Mission has be completed while waiting.', Logger.REPORT_HINT)
-            #Envoi d'un message de synchronisation
+            Logger.log('Mission has be completed while waiting. Synching the submarines', Logger.REPORT_HINT)
+            
+            self.sync = rospy.Publisher('/proc_underwater_com/sync_send_msg', Bool, queue_size=2)
+
+            sync_msg = Bool()
+            sync_msg.data = True
+            self.sync.publish(sync_msg)
+
             self.sync_in_progress = True
         
     def on_enter(self, userdata):
         self.other_receive_array = rospy.Subscriber('/proc_underwater_com/other_sub_mission_list', Int8MultiArray, self.other_mission_array_cb)
         self.time_start = time()
-        self.done_already = False
-        self.failed = False
-        self.mission_to_do = False
-        self.sync_in_progress = False
         
     def execute(self, userdata):
         time_since = time() - self.time_start
@@ -90,7 +157,19 @@ class synchro_master(EventState):
         if self.done_already == True or self.failed == True:
             return 'failed'
         if self.sync_in_progress == True:
-            return 'continue'     
+            return 'continue'
+        if self.request_completed == True and self.depth_received == True and self.other_sub_depth_received == True:
+            #Stopping to get in the service calling in the callback for the other sub mission list
+            self.request_completed = False
+            self.get_current_position_sub.unregister()
+            self.get_depth_other_sub.unregister()
+            
+            # Get the pose to change the depth of this submarine
+            pose = self.profondeur_change_request()
+            
+            if self.failed == False and pose.position.z != 0:
+                userdata.depth_change = pose
+                return 'pose_change_depth'
 
     def on_exit(self, userdata):
         self.other_receive_array.unregister()
--- sonia_flexbe_behaviors/src/sonia_flexbe_behaviors/synchro_master_sm.py
+++ sonia_flexbe_behaviors/src/sonia_flexbe_behaviors/synchro_master_sm.py
@@ -9,6 +9,7 @@
 
 from flexbe_core import Behavior, Autonomy, OperatableStateMachine, ConcurrencyContainer, PriorityContainer, Logger
 from sonia_com_states.synchro_master import synchro_master
+from sonia_flexbe_behaviors.move_with_addpose_msg_sm import MovewithAddposemsgSM
 # Additional imports can be added inside the following tags
 # [MANUAL_IMPORT]
 
@@ -32,6 +33,7 @@ class SynchroMasterSM(Behavior):
 		# parameters of this behavior
 
 		# references to used behaviors
+		self.add_behavior(MovewithAddposemsgSM, 'Move with Addpose msg')
 
 		# Additional initialization code can be added inside the following tags
 		# [MANUAL_INIT]
@@ -43,7 +45,7 @@ class SynchroMasterSM(Behavior):
 
 
 	def create(self):
-		# x:574 y:344, x:579 y:186, x:572 y:57
+		# x:791 y:272, x:342 y:253, x:572 y:57
 		_state_machine = OperatableStateMachine(outcomes=['finished', 'failed', 'timeout'])
 
 		# Additional creation code can be added inside the following tags
@@ -55,9 +57,17 @@ class SynchroMasterSM(Behavior):
 		with _state_machine:
 			# x:157 y:64
 			OperatableStateMachine.add('sync',
-										synchro_master(mission_id=2, timeout=300),
-										transitions={'continue': 'finished', 'failed': 'failed', 'timeout': 'timeout'},
-										autonomy={'continue': Autonomy.Off, 'failed': Autonomy.Off, 'timeout': Autonomy.Off})
+										synchro_master(depth_change=True, max_depth_surface=0.5, max_depth_bottom=1.5, min_depth_offset=1, mission_id=2, timeout=180),
+										transitions={'continue': 'finished', 'failed': 'failed', 'timeout': 'timeout', 'pose_change_depth': 'Move with Addpose msg'},
+										autonomy={'continue': Autonomy.Off, 'failed': Autonomy.Off, 'timeout': Autonomy.Off, 'pose_change_depth': Autonomy.Off},
+										remapping={'depth_change': 'depth_change'})
+
+			# x:92 y:433
+			OperatableStateMachine.add('Move with Addpose msg',
+										self.use_behavior(MovewithAddposemsgSM, 'Move with Addpose msg'),
+										transitions={'finished': 'sync', 'failed': 'failed', 'failed_target_reached': 'failed'},
+										autonomy={'finished': Autonomy.Inherit, 'failed': Autonomy.Inherit, 'failed_target_reached': Autonomy.Inherit},
+										remapping={'pose': 'depth_change'})
 
 
 		return _state_machine
