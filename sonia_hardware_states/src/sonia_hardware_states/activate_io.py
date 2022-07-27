75150d784a66d441f043af7402982ca214adc976
Kam0ss <camille.sauvain.1@etsmtl.net>
2022-07-27 10:30:05 -0400

    activate_io fixed to work with new proc_actuators

--- sonia_hardware_states/src/sonia_hardware_states/activate_io.py
+++ sonia_hardware_states/src/sonia_hardware_states/activate_io.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python 
 #-*- coding: utf-8 -*-
 
+from time import time
 import rospy
 
 from flexbe_core import EventState, Logger
-from sonia_common.msg import ActuatorDoAction
+from sonia_common.msg import ActuatorDoAction, ActuatorSendReply, MissionTimer
+from sonia_navigation_states.modules.navigation_utilities import missionTimerFunc
 
 class activate_io(EventState):
 
@@ -16,28 +18,52 @@ class activate_io(EventState):
         -- action               uint8       see ActuatorDoAction message
 
         <= continue                         Activation of the element successful
-        <= failed                           Failed to activate the element (service issue)
+        <= failed                           Failed to activate the element (Most likely due to kill switch being on)
+        <= timed out                        Activation timed out
 
     '''
 
-    def __init__(self, element, side, action, timeout=10):
-        super(activate_io, self).__init__(outcomes=['continue', 'failed'])
+    def __init__(self, element, side, action, timeout=8):
+        super(activate_io, self).__init__(outcomes=['continue', 'failed', "timed out"])
         
-        self.start_time = None
-        self.param_side = side
-        self.param_element = element
-        self.param_action = action
+        self.time_start = None
+        self.element = element
+        self.side = side
+        self.action = action
         self.timeout = timeout
+        self.response = ""
 
         self.action_pub = rospy.Publisher('/provider_actuators/do_action_to_actuators', ActuatorDoAction, queue_size=2)
+        self.timeout_pub = rospy.Publisher('/sonia_behaviors/timeout', MissionTimer, queue_size=5)
 
     def on_enter(self, userdata):
-        self.action_pub.publish(ActuatorDoAction(self.param_element, self.param_side, self.param_action))
-        
+        self.action_sub = rospy.Subscriber('/provider_actuators/do_action_from_actuators', ActuatorSendReply, self.reply)
+        self.action_pub.publish(ActuatorDoAction(self.element, self.side, self.action))
+        self.time_start = time()
+        self.timeout_pub.publish(missionTimerFunc(f"activate_io (element{self.element}, side{self.side})", self.timeout, str(self.time_start), 1))
+        rospy.loginfo(f'Action : {int(self.element)} is launch')
+
+    def reply(self, data):
+        if data.element == self.element and self.side == data.side:
+            if data.response == ActuatorSendReply.RESPONSE_FAILURE:
+                self.timeout_pub.publish(missionTimerFunc("activate_io (element{self.element}, side{self.side})", self.timeout, str(self.time_start), 4))
+                self.response = "failed"
+            elif data.response == ActuatorSendReply.RESPONSE_SUCCESS:
+                self.timeout_pub.publish(missionTimerFunc("activate_io (element{self.element}, side{self.side})", self.timeout, str(self.time_start), 2))
+                self.response = "continue"
+            elif data.response == ActuatorSendReply.RESPONSE_TIMED_OUT:
+                self.timeout_pub.publish(missionTimerFunc("activate_io (element{self.element}, side{self.side})", self.timeout, str(self.time_start), 3))
+                self.response = "timed out"
+            else:
+                rospy.logerr(f"/provider_actuators/do_action_from_actuators sent a message with the response code {data.response} which should not exist")
 
     def execute(self, userdata):
-        rospy.loginfo('Action : %i is launch' % int(self.param_element))
-        return 'continue'
+        if self.response != "":
+            return self.response
+        if time() - self.time_start >= self.timeout:
+            self.timeout_pub.publish(missionTimerFunc("activate_io (element{self.element}, side{self.side})", self.timeout, str(self.time_start), 3))
+            Logger.log('Timeout Reached', Logger.REPORT_HINT)
+            return "timed out"
 
     def end(self, userdata):
-        pass
+        self.action_sub.unregister()
