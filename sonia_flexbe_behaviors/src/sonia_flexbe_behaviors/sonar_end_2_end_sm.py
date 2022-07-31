66028eb5a7965037a9ecc63f94a71ef5d4142687
Kam0ss <camille.sauvain.1@etsmtl.net>
2022-07-31 17:33:18 -0400

    fix sonar end 2 end

--- sonia_flexbe_behaviors/src/sonia_flexbe_behaviors/sonar_end_2_end_sm.py
+++ sonia_flexbe_behaviors/src/sonia_flexbe_behaviors/sonar_end_2_end_sm.py
@@ -8,11 +8,13 @@
 ###########################################################
 
 from flexbe_core import Behavior, Autonomy, OperatableStateMachine, ConcurrencyContainer, PriorityContainer, Logger
+from sonia_flexbe_states.activate_behavior import activate_behavior
 from sonia_mapping_states.sonar_config import sonar_config
 from sonia_mapping_states.start_bundle import start_bundle
 from sonia_mapping_states.start_stop_sonar import start_stop_sonar
 from sonia_mapping_states.stop_sonar_bundle import stop_sonar_bundle
 from sonia_navigation_states.init_trajectory import init_trajectory
+from sonia_navigation_states.is_moving import is_moving
 from sonia_navigation_states.manual_add_pose_to_trajectory import manual_add_pose_to_trajectory
 from sonia_navigation_states.send_to_planner import send_to_planner
 from sonia_navigation_states.wait_target_reached import wait_target_reached
@@ -37,6 +39,9 @@ class sonar_end_2_endSM(Behavior):
 		self.name = 'sonar_end_2_end'
 
 		# parameters of this behavior
+		self.add_parameter('simulation', False)
+		self.add_parameter('dist_to_torpedoes_board', 1)
+		self.add_parameter('y_offset', 0.4)
 
 		# references to used behaviors
 
@@ -60,18 +65,11 @@ class sonar_end_2_endSM(Behavior):
 
 
 		with _state_machine:
-			# x:74 y:81
+			# x:8 y:70
 			OperatableStateMachine.add('start sonar',
 										start_stop_sonar(startStop=True),
-										transitions={'continue': 'set_config'},
+										transitions={'continue': 'simulation'},
 										autonomy={'continue': Autonomy.Off})
-
-			# x:108 y:427
-			OperatableStateMachine.add('allign',
-										manual_add_pose_to_trajectory(positionX=-1.0, positionY=0.0, positionZ=0.0, orientationX=0.0, orientationY=0.0, orientationZ=0.0, frame=12, speed=0, precision=0, long_rotation=False),
-										transitions={'continue': 'RAM'},
-										autonomy={'continue': Autonomy.Off},
-										remapping={'input_traj': 'trajectory', 'trajectory': 'trajectory'})
 
 			# x:728 y:91
 			OperatableStateMachine.add('down',
@@ -83,7 +81,7 @@ class sonar_end_2_endSM(Behavior):
 			# x:514 y:81
 			OperatableStateMachine.add('init1',
 										init_trajectory(interpolation_method=0),
-										transitions={'continue': 'down'},
+										transitions={'continue': 'set_initial_depth'},
 										autonomy={'continue': Autonomy.Off},
 										remapping={'trajectory': 'trajectory'})
 
@@ -94,12 +92,11 @@ class sonar_end_2_endSM(Behavior):
 										autonomy={'continue': Autonomy.Off},
 										remapping={'trajectory': 'trajectory'})
 
-			# x:452 y:545
-			OperatableStateMachine.add('retract',
-										manual_add_pose_to_trajectory(positionX=-1.0, positionY=0.0, positionZ=0.0, orientationX=0.0, orientationY=0.0, orientationZ=0.0, frame=12, speed=0, precision=0, long_rotation=False),
-										transitions={'continue': 'sned2'},
-										autonomy={'continue': Autonomy.Off},
-										remapping={'input_traj': 'trajectory', 'trajectory': 'trajectory'})
+			# x:1073 y:397
+			OperatableStateMachine.add('is_moving',
+										is_moving(timeout=30, tolerance=0.1),
+										transitions={'stopped': 'finished', 'moving': 'tg2', 'error': 'failed'},
+										autonomy={'stopped': Autonomy.Off, 'moving': Autonomy.Off, 'error': Autonomy.Off})
 
 			# x:731 y:241
 			OperatableStateMachine.add('send',
@@ -114,6 +111,19 @@ class sonar_end_2_endSM(Behavior):
 										transitions={'continue': 'start bundle'},
 										autonomy={'continue': Autonomy.Off})
 
+			# x:730 y:12
+			OperatableStateMachine.add('set_initial_depth',
+										manual_add_pose_to_trajectory(positionX=0.0, positionY=0.0, positionZ=1, orientationX=0.0, orientationY=0.0, orientationZ=0.0, frame=4, speed=0, precision=0, long_rotation=False),
+										transitions={'continue': 'down'},
+										autonomy={'continue': Autonomy.Off},
+										remapping={'input_traj': 'trajectory', 'trajectory': 'trajectory'})
+
+			# x:187 y:38
+			OperatableStateMachine.add('simulation',
+										activate_behavior(activate=self.simulation),
+										transitions={'activate': 'start bundle', 'desactivate': 'set_config'},
+										autonomy={'activate': Autonomy.Off, 'desactivate': Autonomy.Off})
+
 			# x:694 y:540
 			OperatableStateMachine.add('sned2',
 										send_to_planner(),
@@ -121,7 +131,7 @@ class sonar_end_2_endSM(Behavior):
 										autonomy={'continue': Autonomy.Off, 'failed': Autonomy.Off},
 										remapping={'input_traj': 'trajectory'})
 
-			# x:322 y:81
+			# x:336 y:81
 			OperatableStateMachine.add('start bundle',
 										start_bundle(sonarBundle=True, hydroBundle=False, sonarTarget='Buoys', hydroTarget=20, resetSonarBundle=True, resetHydroBundle=False),
 										transitions={'continue': 'init1'},
@@ -148,7 +158,7 @@ class sonar_end_2_endSM(Behavior):
 			# x:876 y:531
 			OperatableStateMachine.add('tg2',
 										wait_target_reached(timeout=30),
-										transitions={'target_reached': 'finished', 'target_not_reached': 'failed', 'error': 'failed'},
+										transitions={'target_reached': 'finished', 'target_not_reached': 'is_moving', 'error': 'failed'},
 										autonomy={'target_reached': Autonomy.Off, 'target_not_reached': Autonomy.Off, 'error': Autonomy.Off})
 
 			# x:730 y:166
@@ -158,10 +168,10 @@ class sonar_end_2_endSM(Behavior):
 										autonomy={'continue': Autonomy.Off},
 										remapping={'input_traj': 'trajectory', 'trajectory': 'trajectory'})
 
-			# x:171 y:549
-			OperatableStateMachine.add('RAM',
-										manual_add_pose_to_trajectory(positionX=0.20, positionY=0.0, positionZ=0.0, orientationX=0.0, orientationY=0.0, orientationZ=0.0, frame=12, speed=0, precision=0, long_rotation=False),
-										transitions={'continue': 'retract'},
+			# x:108 y:427
+			OperatableStateMachine.add('allign',
+										manual_add_pose_to_trajectory(positionX=-self.dist_to_torpedoes_board, positionY=self.y_offset, positionZ=0.0, orientationX=0.0, orientationY=0.0, orientationZ=0.0, frame=12, speed=0, precision=0, long_rotation=False),
+										transitions={'continue': 'sned2'},
 										autonomy={'continue': Autonomy.Off},
 										remapping={'input_traj': 'trajectory', 'trajectory': 'trajectory'})
 
