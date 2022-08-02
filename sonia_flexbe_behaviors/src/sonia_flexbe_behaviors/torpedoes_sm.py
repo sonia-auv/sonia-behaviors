3cda077ac020d178816b10af5db9bf08d9db10fc
Willy <willy.kao.1@ens.etsmtl.ca>
2022-08-02 03:18:29 -0400

    accelerated alignement for torpedoes

--- sonia_flexbe_behaviors/src/sonia_flexbe_behaviors/torpedoes_sm.py
+++ sonia_flexbe_behaviors/src/sonia_flexbe_behaviors/torpedoes_sm.py
@@ -10,7 +10,7 @@
 from flexbe_core import Behavior, Autonomy, OperatableStateMachine, ConcurrencyContainer, PriorityContainer, Logger
 from sonia_flexbe_behaviors.launch_auv8_sm import launch_AUV8SM
 from sonia_flexbe_behaviors.move_sm import moveSM
-from sonia_flexbe_behaviors.vision_torpedoes_boards_sm import vision_torpedoes_boardsSM
+from sonia_flexbe_behaviors.vision_torpedoes_boards_new_sm import vision_torpedoes_boards_newSM
 from sonia_flexbe_behaviors.vision_torpedoes_sm import vision_torpedoesSM
 # Additional imports can be added inside the following tags
 # [MANUAL_IMPORT]
@@ -38,7 +38,7 @@ class torpedoesSM(Behavior):
 		self.add_behavior(launch_AUV8SM, 'launch_AUV8')
 		self.add_behavior(moveSM, 'move')
 		self.add_behavior(vision_torpedoesSM, 'vision_torpedoes')
-		self.add_behavior(vision_torpedoes_boardsSM, 'vision_torpedoes_boards')
+		self.add_behavior(vision_torpedoes_boards_newSM, 'vision_torpedoes_boards_new')
 
 		# Additional initialization code can be added inside the following tags
 		# [MANUAL_INIT]
@@ -61,13 +61,12 @@ class torpedoesSM(Behavior):
 
 		with _state_machine:
 			# x:30 y:40
-			OperatableStateMachine.add('vision_torpedoes_boards',
-										self.use_behavior(vision_torpedoes_boardsSM, 'vision_torpedoes_boards',
-											parameters={'vision_torpedoes_boards_target': "G-Man", 'bounding_box_width': 150, 'bounding_box_height': 200, 'center_bounding_box_width': 100, 'center_bounding_box_height': 100, 'max_mouvement': 2, 'min_mouvement': 0.25, 'activate_vision_buoys': True}),
+			OperatableStateMachine.add('vision_torpedoes_boards_new',
+										self.use_behavior(vision_torpedoes_boards_newSM, 'vision_torpedoes_boards_new'),
 										transitions={'finished': 'move', 'failed': 'vision_torpedoes', 'lost_target': 'move'},
 										autonomy={'finished': Autonomy.Inherit, 'failed': Autonomy.Inherit, 'lost_target': Autonomy.Inherit})
 
-			# x:287 y:50
+			# x:112 y:163
 			OperatableStateMachine.add('move',
 										self.use_behavior(moveSM, 'move',
 											parameters={'positionX': 1, 'positionY': 0, 'positionZ': 0, 'orientationX': 0, 'orientationY': 0, 'orientationZ': 0, 'frame': 1, 'speed': 0, 'precision': 0, 'rotation': True}),
