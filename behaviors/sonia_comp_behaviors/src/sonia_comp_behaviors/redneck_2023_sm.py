fd38f7dcb803d4c4a46ef8791afd481bbca1c12c
Willy <willy.kao.1@ens.etsmtl.ca>
2023-08-04 17:58:22 -0400

    YOLO

--- behaviors/sonia_comp_behaviors/src/sonia_comp_behaviors/redneck_2023_sm.py
+++ behaviors/sonia_comp_behaviors/src/sonia_comp_behaviors/redneck_2023_sm.py
@@ -12,6 +12,8 @@ from sonia_base_behaviors.coin_flip_tare_sm import coin_flip_tareSM
 from sonia_base_behaviors.single_pose_move_sm import SinglePoseMoveSM
 from sonia_base_behaviors.trick_shot_pitch_roll_sm import trick_shot_pitch_rollSM
 from sonia_complex_behaviors.align_gate_angle_sm import AlignGateAngleSM
+from sonia_hardware_states.wait_mission import wait_mission
+from sonia_navigation_states.set_control_mode import set_control_mode
 from sonia_vision_states.get_buoy import get_buoy
 # Additional imports can be added inside the following tags
 # [MANUAL_IMPORT]
@@ -40,6 +42,10 @@ class Redneck2023SM(Behavior):
 		self.add_behavior(AlignGateAngleSM, 'Align Gate Angle')
 		self.add_behavior(SinglePoseMoveSM, 'Dive to 1.3m move 3m')
 		self.add_behavior(SinglePoseMoveSM, 'Move 5x, -0.2z after Coin Flip')
+		self.add_behavior(SinglePoseMoveSM, 'Single Pose Move')
+		self.add_behavior(SinglePoseMoveSM, 'Single Pose Move_2')
+		self.add_behavior(SinglePoseMoveSM, 'Single Pose Move_2_2')
+		self.add_behavior(SinglePoseMoveSM, 'Single Pose Move_3')
 		self.add_behavior(coin_flip_tareSM, 'coin_flip_tare')
 		self.add_behavior(trick_shot_pitch_rollSM, 'trick_shot_pitch_roll')
 
@@ -67,12 +73,11 @@ class Redneck2023SM(Behavior):
 
 
 		with _state_machine:
-			# x:134 y:21
-			OperatableStateMachine.add('coin_flip_tare',
-										self.use_behavior(coin_flip_tareSM, 'coin_flip_tare',
-											parameters={'orientation_to_gate': 0, 'dive_depth': 0, 'activate_coin_flip': True}),
-										transitions={'finished': 'Move 5x, -0.2z after Coin Flip', 'failed': 'failed'},
-										autonomy={'finished': Autonomy.Inherit, 'failed': Autonomy.Inherit})
+			# x:30 y:89
+			OperatableStateMachine.add('wait mission',
+										wait_mission(),
+										transitions={'continue': 'coin_flip_tare'},
+										autonomy={'continue': Autonomy.Off})
 
 			# x:657 y:262
 			OperatableStateMachine.add('Dive to 1.3m move 3m',
@@ -84,7 +89,7 @@ class Redneck2023SM(Behavior):
 			# x:380 y:478
 			OperatableStateMachine.add('Get Buoy',
 										get_buoy(),
-										transitions={'success': 'finished', 'failed': 'failed'},
+										transitions={'success': 'Single Pose Move', 'failed': 'failed'},
 										autonomy={'success': Autonomy.Off, 'failed': Autonomy.Off},
 										remapping={'average_size': 'average_size', 'average_ctr': 'average_ctr'})
 
@@ -94,6 +99,47 @@ class Redneck2023SM(Behavior):
 											parameters={'positionX': 4, 'positionY': 0.0, 'positionZ': -0.4, 'orientationZ': 0.0}),
 										transitions={'finished': 'Align Gate Angle', 'failed': 'failed'},
 										autonomy={'finished': Autonomy.Inherit, 'failed': Autonomy.Inherit})
+
+			# x:182 y:436
+			OperatableStateMachine.add('Single Pose Move',
+										self.use_behavior(SinglePoseMoveSM, 'Single Pose Move',
+											parameters={'positionX': 1.5}),
+										transitions={'finished': 'Single Pose Move_2', 'failed': 'failed'},
+										autonomy={'finished': Autonomy.Inherit, 'failed': Autonomy.Inherit})
+
+			# x:237 y:557
+			OperatableStateMachine.add('Single Pose Move_2',
+										self.use_behavior(SinglePoseMoveSM, 'Single Pose Move_2',
+											parameters={'positionX': -1.5}),
+										transitions={'finished': 'Single Pose Move_3', 'failed': 'failed'},
+										autonomy={'finished': Autonomy.Inherit, 'failed': Autonomy.Inherit})
+
+			# x:82 y:295
+			OperatableStateMachine.add('Single Pose Move_2_2',
+										self.use_behavior(SinglePoseMoveSM, 'Single Pose Move_2_2',
+											parameters={'positionX': -1.5}),
+										transitions={'finished': 'kill', 'failed': 'failed'},
+										autonomy={'finished': Autonomy.Inherit, 'failed': Autonomy.Inherit})
+
+			# x:20 y:565
+			OperatableStateMachine.add('Single Pose Move_3',
+										self.use_behavior(SinglePoseMoveSM, 'Single Pose Move_3',
+											parameters={'positionX': 1.5, 'positionZ': 0.5}),
+										transitions={'finished': 'Single Pose Move_2_2', 'failed': 'failed'},
+										autonomy={'finished': Autonomy.Inherit, 'failed': Autonomy.Inherit})
+
+			# x:134 y:21
+			OperatableStateMachine.add('coin_flip_tare',
+										self.use_behavior(coin_flip_tareSM, 'coin_flip_tare',
+											parameters={'orientation_to_gate': 0, 'dive_depth': 0, 'activate_coin_flip': True}),
+										transitions={'finished': 'Move 5x, -0.2z after Coin Flip', 'failed': 'failed'},
+										autonomy={'finished': Autonomy.Inherit, 'failed': Autonomy.Inherit})
+
+			# x:30 y:151
+			OperatableStateMachine.add('kill',
+										set_control_mode(mode=0, timeout=5),
+										transitions={'continue': 'finished', 'failed': 'failed'},
+										autonomy={'continue': Autonomy.Off, 'failed': Autonomy.Off})
 
 			# x:644 y:404
 			OperatableStateMachine.add('trick_shot_pitch_roll',
