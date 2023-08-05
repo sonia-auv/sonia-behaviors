08593d3badcab791837560b20d6d8f622d4879a4
Willy <willy.kao.1@ens.etsmtl.ca>
2023-08-05 15:10:47 -0400

    stuff

--- states/sonia_vision_states/src/sonia_vision_states/calc_ctr_ai.py
+++ states/sonia_vision_states/src/sonia_vision_states/calc_ctr_ai.py
@@ -5,7 +5,7 @@ class calculate_ctr_ai(EventState):
     def __init__(self):
         super().__init__(outcomes=["success"],
                          input_keys=["ai_pos"],
-                         output_keys=["obj_ctr"])
+                         output_keys=["obj_ctr", "obj_area"])
 
     def execute(self, userdata):
         top = userdata.ai_pos[0]
@@ -13,7 +13,12 @@ class calculate_ctr_ai(EventState):
         right = userdata.ai_pos[2]
         bottom = userdata.ai_pos[3]
 
+        area = (right-left)*(bottom-top)
+
+
+
         avg_x = (left + right) / 2
         avg_y = (top + bottom) / 2
         userdata.obj_ctr = (avg_x, avg_y)
+        userdata.obj_area = area
         return "success"
